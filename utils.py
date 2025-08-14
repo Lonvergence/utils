07030f9d5e9fed0f5935d2226ee233cfc3083ff7
Lonvergence <1156997364@qq.com>
2025-08-14 17:33:45 +0800

    add RegionMap

--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ import SimpleITK as sitk
 import skimage
 from loguru import logger
 
+from collections import Counter, defaultdict
+from copy import deepcopy
+
 def check_shape_equal(*mats):
     mat0 = mats[0]
     if not all([m.shape == mat0.shape for m in mats[1:]]):
@@ -93,5 +96,66 @@ class Accumulator:
 
 
 
+class RegionMap:
+    def __init__(self, json_path):
+        import json
+        with open(json_path, 'r') as f:
+            self.cfg = json.load(f)
+        self._all_attr = self._get_all_attr()
+    
+    def _get_all_attr(self):
+        return self.cfg.keys()
+    
+    def get_cfg_by_attr(self, attr, value):
+        if attr not in self._all_attr:
+            raise ValueError(f"Attribute {attr} not found in the configuration.")
+        cfg = self.cfg
+        def recur(cfg):
+            if cfg.get(attr) == value:
+                return cfg
+            for child in cfg.get("children", []):
+                result = recur(child)
+                if result:
+                    return result
+            return None
+        return recur(cfg)
+
+    
+    def get_all_sub(self, attr1, value, attr2):
+        # 得到attr1为value的脑区下面所有子脑区的attr2
+        res = []
+        cfg = self.get_cfg_by_attr(attr1, value)
+
+        def recur(cfg, attr2, res):
+            res.append(cfg[attr2])
+            for child in cfg.get("children", []):
+                recur(child, attr2, res)
+        recur(cfg, attr2, res)
+        return res
+
+    def cal_region_areas(self, arr):
+        # 递归计算所有区域的大小
+
+        cfg = self.cfg
+        dis = Counter(arr.flatten())
+        def recur(cfg, dis):
+            if cfg["id_ytw"] not in dis.keys():
+                dis[cfg["id_ytw"]] = 0
+            
+            for child in cfg["children"]:
+                res = recur(child, dis)
+                dis[cfg["id_ytw"]] += res
+
+            return  dis[cfg["id_ytw"]]
+        
+        recur(cfg, dis)
+
+        return dis
 
 
+
+
+if __name__ == "__main__":
+    region_map = RegionMap("./add_id_ytw.json")
+    print(region_map.get_cfg_by_attr("name", "ventral tegmental decussation"))
+    
